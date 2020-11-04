114ef51371f89699aa5fc9558de91a70f6c4bc47
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2020-11-04 11:30:05 +0100

    add genre display

--- scripts/featured.js
+++ scripts/featured.js
@@ -1,20 +1,47 @@
 const featuredGrid = document.getElementById('featuredGrid');
 const templateFeaturedFilm = document.getElementById('templateFeaturedFilm')
 
-function addFeaturedFilm(data){
-    for(let i=0;i<20;i++){
+function addFeaturedFilm(featured, genres){
+    for(let i=0;i<10;i++){
         let newItem = document.createElement('div');
         newItem.innerHTML =templateFeaturedFilm.innerHTML;
-        newItem.querySelector('h3').innerText = data.results[i].title
-        newItem.querySelector('.year').innerText = data.results[i].release_date.slice(0,4)
-        newItem.querySelector('img').setAttribute('src', `http://image.tmdb.org/t/p/w185/${data.results[i].poster_path}`)
+        newItem.querySelector('h3').innerText = featured.results[i].title
+        newItem.querySelector('.year').innerText = featured.results[i].release_date.slice(0,4) + ' - ';
+        findGenre(featured.results[i].genre_ids, genres.genres);
+        newItem.querySelector('.genre').innerText = `${genre[i]}`
+        newItem.querySelector('img').setAttribute('src', `http://image.tmdb.org/t/p/w300/${featured.results[i].poster_path}`)
         featuredGrid.appendChild(newItem);
     }
 }
 
-fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=112ca2b68890cedc6cd02c2b81593072')
+let genre = []
+
+function findGenre(filmGenreID, genres){
+    filmGenreID.map((id)=>{
+        for (i=0; i<genres.length;i++){
+            if (id===genres[i].id){
+                console.log(genres[i].name)
+                genre.push(genres[i].name)
+            }
+        }
+    })
+}
+
+/* const fetchFeatured = fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=112ca2b68890cedc6cd02c2b81593072')
 .then(res=>res.json())
 .then(data => {
     console.log(data.results);
     addFeaturedFilm(data)
+}) */
+
+const fetchFeatured = fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=112ca2b68890cedc6cd02c2b81593072');
+const fetchGenres = fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=112ca2b68890cedc6cd02c2b81593072&language=en-US');
+
+Promise.all([fetchFeatured, fetchGenres])
+.then(values=>{
+    return Promise.all(values.map(v=>v.json()))
+}).then(([featured,genres])=>{
+    console.log(featured)
+    console.log(genres)
+    addFeaturedFilm(featured, genres)
 })
